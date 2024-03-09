fa88e0def88f5e4d2b77155082c3bff131ffab41
SirTonyEdgar <121780770+SirTonyEdgar@users.noreply.github.com>
2024-03-09 09:11:00 +0700

    Update Form1.cs

--- modul3_1302223092/Form1.cs
+++ modul3_1302223092/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +7,96 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace modul3_1302223092
 {
     public partial class Form1 : Form
     {
+        string angka1;
+        int total;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "1 ";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "2 ";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "3 ";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "4 ";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "5 ";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "6 ";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "7 ";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "8 ";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "9 ";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            angka1 = textBox1.Text;
+            textBox1.Text = null;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "0 ";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            total = Int32.Parse(angka1) + Int32.Parse(textBox1.Text);
+            textBox1.Text = Convert.ToString(total);
+            angka1 = textBox1.Text;
         }
     }
 }
